5c294e4040544524362a764bcecb8ac6b87aac47
Shlef <mohamedsaidwork@gmail.com>
2022-08-15 11:06:57 +0200

    Main page style

---
+++ src/script/script.js
@@ -0,0 +1,11 @@
+const cards = [...document.querySelectorAll('.projects-container .card')];
+
+cards.forEach((card) => {
+  card.addEventListener('mousemove', (e) => {
+    const x = e.pageX - card.offsetLeft;
+    const y = e.pageY - card.offsetTop;
+
+    card.style.setProperty('--x', x + 'px');
+    card.style.setProperty('--y', y + 'px');
+  });
+});
